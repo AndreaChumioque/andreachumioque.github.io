aa217ffd61a0b1a014674c1f98eba6e92dab0e4c
andrea-isabel <andrea19.93.15@gmail.com>
2018-02-19 04:19:19 -0500

    cambios

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 'use strict';
 
-var init = function init() {
+$(window).on('load', function () {
   // Función menú hamburguesa
-  $('#nav-icon4').click(function(event) {
+  $('#nav-icon4').click(function (event) {
     $('#nav-icon4').toggleClass('open');
   });
 
   // Smooth scroll
-  $('header a').click(function(event) {
+  $('header a').click(function (event) {
     event.preventDefault();
-    $('html, body').stop().animate({ scrollTop: $($(event.target).attr('href')).offset().top }, 1000);
+    $('html, body').stop().animate({ scrollTop: $($(event.target).attr('href')).offset().top - 70 }, 1000);
   });
-};
 
-window.addEventListener('load', init);
+  $(function () {
+    Grid.init();
+  });
+});
