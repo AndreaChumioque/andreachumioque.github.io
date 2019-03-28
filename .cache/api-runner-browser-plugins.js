1cbb76f7bd87697d723ea3547a58709b34ac0816
andreachumioque <aichumioque@gmail.com>
2019-03-28 13:32:31 -0500

    yarn integrity

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('C:/Users/andre/Documents/Andrea/Laboratoria/andreachumioque.github.io/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      options: {"plugins":[],"pathToConfigModule":"C:\\Users\\andre\\Documents\\Andrea\\Laboratoria\\andreachumioque.github.io/src/utils/typography"},
+    },{
+      plugin: require('C:/Users/andre/Documents/Andrea/Laboratoria/andreachumioque.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,14 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("C:\\Users\\andre\\Documents\\Andrea\\Laboratoria\\andreachumioque.github.io\\.cache\\dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("C:\\Users\\andre\\Documents\\Andrea\\Laboratoria\\andreachumioque.github.io\\src\\pages\\404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("C:\\Users\\andre\\Documents\\Andrea\\Laboratoria\\andreachumioque.github.io\\src\\pages\\about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-contact-js": () => import("C:\\Users\\andre\\Documents\\Andrea\\Laboratoria\\andreachumioque.github.io\\src\\pages\\contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-js": () => import("C:\\Users\\andre\\Documents\\Andrea\\Laboratoria\\andreachumioque.github.io\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-portfolio-js": () => import("C:\\Users\\andre\\Documents\\Andrea\\Laboratoria\\andreachumioque.github.io\\src\\pages\\portfolio.js" /* webpackChunkName: "component---src-pages-portfolio-js" */)
+}
+
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "C:\\Users\\andre\\Documents\\Andrea\\Laboratoria\\andreachumioque.github.io\\.cache\\data.json")
+
